4c111e33dba81a89abd7b365ac5571f52622be10
Seth VanHeulen <svanheulen@structart.com>
2014-03-29 12:56:34 -0400

    fixed fatigue date calculation, simplified many functions

--- digger.lua
+++ digger.lua
@@ -16,19 +16,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 _addon.name = 'digger'
-_addon.version = '1.0.1'
+_addon.version = '1.1.0'
 _addon.command = 'digger'
 _addon.author = 'Seth VanHeulen'
-
-function get_jst_date()
-    jst_date = os.time(os.date('!*t')) + (9 * 60 * 60)
-    return os.date('%Y-%m-%d', jst_date)
-end
 
 defaults = {}
 defaults.rank = 'amateur'
 defaults.fatigue = {}
-defaults.fatigue.date = get_jst_date()
+defaults.fatigue.date = os.date('%Y-%m-%d', os.time() + 32400)
 defaults.fatigue.remaining = 100
 defaults.accuracy = {}
 defaults.accuracy.successful = 0
@@ -44,7 +39,21 @@ dig_delay = 15
 fail_message_ids = {7032, 7191, 7195, 7205, 7213, 7224, 7247, 7253, 7533, 7679}
 success_message_ids = {6379, 6393, 6406, 6552, 7372, 7687, 7712}
 ease_message_ids = {7107, 7266, 7270, 7280, 7288, 7299, 7322, 7328, 7608, 7754}
-rental_zones = {51, 82, 90, 97, 102, 108, 114, 117, 124}
+
+-- binary helper functions
+
+function string.unpack_uint16(str, i)
+    return str:byte(i + 1) * 0x100 + str:byte(i)
+end
+
+function string.unpack_uint32(str, i)
+    local num = str:byte(i + 3)
+    num = num * 0x100 + str:byte(i + 2)
+    num = num * 0x100 + str:byte(i + 1)
+    return num * 0x100 + str:byte(i)
+end
+
+-- buff helper function
 
 function get_chocobo_buff()
     for _,buff_id in pairs(windower.ffxi.get_player().buffs) do
@@ -55,70 +64,72 @@ function get_chocobo_buff()
     return false
 end
 
-function check_zone_change(new_id, old_id)
-    for _,rental_zone in pairs(rental_zones) do
-        if new_id == rental_zone then
-            windower.send_command(string.format('timers c "Chocobo Area Delay" %d down', area_delay + area_delay_lag))
-            return
-        end
-    end
-    if get_chocobo_buff() == true then
-        windower.send_command(string.format('timers c "Chocobo Area Delay" %d down', area_delay + area_delay_lag))
-    end
-end
+-- inventory helper function
 
 function get_gysahl_count()
     count = 0
     for _,item in pairs(windower.ffxi.get_items().inventory) do
-        if item.id == 4545 and item.flags ~= 25 then
+        if item.id == 4545 and item.status == 0 then
             count = count + item.count
         end
     end
     return count
 end
 
-function update_fatigue()
-    today = get_jst_date()
+-- stats helper functions
+
+function update_stats(count)
+    today = os.date('%Y-%m-%d', os.time() + 32400)
     if settings.fatigue.date ~= today then
         settings.fatigue.date = today
         settings.fatigue.remaining = 100
     end
+    if count < 1 then
+        settings.accuracy.total = settings.accuracy.total + 1
+    end
+    if count < 0 then
+        settings.accuracy.successful = settings.accuracy.successful + 1
+    end
+    settings.fatigue.remaining = settings.fatigue.remaining + count
+    settings:save('all')
+end
+
+function display_stats()
+    accuracy = (settings.accuracy.successful / settings.accuracy.total) * 100
+    windower.add_to_chat(200, 'dig accuracy: %d%% (%d/%d), items until fatigued: %d, gysahl greens remaining: %d':format(accuracy, settings.accuracy.successful, settings.accuracy.total, settings.fatigue.remaining, get_gysahl_count()))
+end
+
+-- event callback functions
+
+function check_zone_change(new_id, old_id)
+    windower.send_command(string.format('timers c "Chocobo Area Delay" %d down', area_delay + area_delay_lag))
 end
 
 function check_incoming_chunk(id, original, modified, injected, blocked)
     if id == 47 and dig_delay > 0 then
-        player_id = string.byte(original, 5) + string.byte(original, 6) * 256
-        if player_id == windower.ffxi.get_player().id then
+        if windower.ffxi.get_player().id == original:unpack_uint32(5) then
             windower.send_command(string.format('timers c "Chocobo Dig Delay" %d down', dig_delay))
         end
     elseif id == 54 then
-        message_id = string.byte(original, 11) + ((string.byte(original, 12) % 128) * 256)
+        message_id = original:unpack_uint16(11) % 0x8000
         for _,fail_message_id in pairs(fail_message_ids) do
             if message_id == fail_message_id then
-                settings.accuracy.total = settings.accuracy.total + 1
-                update_fatigue()
-                settings:save('all')
-                windower.add_to_chat(207, string.format('dig accuracy: %d%% (%d/%d), items until fatigued: %d, gysahl greens remaining: %d', (settings.accuracy.successful / settings.accuracy.total) * 100, settings.accuracy.successful, settings.accuracy.total, settings.fatigue.remaining, get_gysahl_count()))
+                update_stats(0)
+                display_stats()
             end
         end
     elseif id == 42 then
-        message_id = string.byte(original, 27) + ((string.byte(original, 28) % 128) * 256)
+        message_id = original:unpack_uint16(27) % 0x8000
         for _,success_message_id in pairs(success_message_ids) do
             if message_id == success_message_id and get_chocobo_buff() == true then
-                settings.accuracy.successful = settings.accuracy.successful + 1
-                settings.accuracy.total = settings.accuracy.total + 1
-                update_fatigue()
-                settings.fatigue.remaining = settings.fatigue.remaining - 1
-                settings:save('all')
-                windower.add_to_chat(207, string.format('dig accuracy: %d%% (%d/%d), items until fatigued: %d, gysahl greens remaining: %d', (settings.accuracy.successful / settings.accuracy.total) * 100, settings.accuracy.successful, settings.accuracy.total, settings.fatigue.remaining, get_gysahl_count()))
+                update_stats(-1)
+                display_stats()
                 return
             end
         end
         for _,ease_message_id in pairs(ease_message_ids) do
             if message_id == ease_message_id then
-                update_fatigue()
-                settings.fatigue.remaining = settings.fatigue.remaining + 1
-                settings:save('all')
+                update_stats(1)
             end
         end
     end
@@ -163,14 +174,14 @@ function set_dig_rank(rank)
         windower.add_to_chat(167, string.format('invalid digging rank: %s', rank))
         return false
     end
-    windower.add_to_chat(207, string.format('setting digging rank to %s: area delay = %d seconds, dig delay = %d seconds', rank, area_delay, dig_delay))
+    windower.add_to_chat(200, string.format('setting digging rank to %s: area delay = %d seconds, dig delay = %d seconds', rank, area_delay, dig_delay))
     settings.rank = rank
     return true
 end
 
 function digger_command(...)
     if #arg == 1 and arg[1]:lower() == 'reset' then
-        windower.add_to_chat(207, 'resetting dig accuracy statistics')
+        windower.add_to_chat(200, 'resetting dig accuracy statistics')
         settings.accuracy.successful = 0
         settings.accuracy.total = 0
         settings:save('all')
@@ -187,6 +198,8 @@ end
 function digger_load()
     set_dig_rank(settings.rank)
 end
+
+-- register event callbacks
 
 windower.register_event('zone change', check_zone_change)
 windower.register_event('incoming chunk', check_incoming_chunk)
