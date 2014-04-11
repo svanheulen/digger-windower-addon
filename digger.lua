4a0cc2780a14ab79e164162361f5c0930123c6f2
svanheulen <svanheulen@gmail.com>
2014-04-11 18:41:18 -0400

    simplified message id checks, updated message ids, made variables local

--- digger.lua
+++ digger.lua
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- addon information
 
 _addon.name = 'digger'
-_addon.version = '1.2.1'
+_addon.version = '1.3.0'
 _addon.command = 'digger'
 _addon.author = 'Seth VanHeulen'
 
@@ -45,10 +45,26 @@ settings = config.load(defaults)
 
 -- global constants
 
-fail_message_ids = {7032, 7191, 7195, 7205, 7213, 7224, 7247, 7253, 7533, 7679}
-success_message_ids = {6379, 6393, 6406, 6552, 7372, 7687, 7712}
-ease_message_ids = {7107, 7266, 7270, 7280, 7288, 7299, 7322, 7328, 7608, 7754}
-chocobo_zone_ids = {2, 4, 51, 52, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 114, 115, 116, 117, 118, 119, 120, 121, 123, 124, 125}
+fail_message = {
+    [7208]=true, [7250]=true, [7227]=true, [7536]=true, [7194]=true,
+    [7682]=true, [7198]=true, [7256]=true, [7216]=true, [7035]=true
+}
+success_message = {
+    [6379]=true, [6393]=true, [6406]=true, [6552]=true, [7377]=true,
+    [7692]=true, [7717]=true
+}
+ease_message = {
+    [7283]=true, [7325]=true, [7302]=true, [7611]=true, [7269]=true,
+    [7757]=true, [7273]=true, [7331]=true, [7291]=true, [7110]=true
+}
+chocobo_zone = {
+    [2]=true,   [4]=true,   [51]=true,  [52]=true,  [100]=true,
+    [101]=true, [102]=true, [103]=true, [104]=true, [105]=true,
+    [106]=true, [107]=true, [108]=true, [109]=true, [110]=true,
+    [114]=true, [115]=true, [116]=true, [117]=true, [118]=true,
+    [119]=true, [120]=true, [121]=true, [123]=true, [124]=true,
+    [125]=true
+}
 
 -- buff helper function
 
@@ -64,7 +80,7 @@ end
 -- inventory helper function
 
 function get_gysahl_count()
-    count = 0
+    local count = 0
     for _,item in pairs(windower.ffxi.get_items().inventory) do
         if item.id == 4545 and item.status == 0 then
             count = count + item.count
@@ -76,7 +92,7 @@ end
 -- stats helper functions
 
 function update_stats(count)
-    today = os.date('!%Y-%m-%d', os.time() + 32400)
+    local today = os.date('!%Y-%m-%d', os.time() + 32400)
     if settings.fatigue.date ~= today then
         settings.fatigue.date = today
         settings.fatigue.remaining = 100
@@ -92,45 +108,34 @@ function update_stats(count)
 end
 
 function display_stats()
-    accuracy = (settings.accuracy.successful / settings.accuracy.total) * 100
-    windower.add_to_chat(200, 'dig accuracy: %d%% (%d/%d), items until fatigued: %d, gysahl greens remaining: %d':format(accuracy, settings.accuracy.successful, settings.accuracy.total, settings.fatigue.remaining, get_gysahl_count()))
+    local accuracy = (settings.accuracy.successful / settings.accuracy.total) * 100
+    windower.add_to_chat(207, 'dig accuracy: %d%% (%d/%d), items until fatigued: %d, gysahl greens remaining: %d':format(accuracy, settings.accuracy.successful, settings.accuracy.total, settings.fatigue.remaining, get_gysahl_count()))
 end
 
 -- event callback functions
 
-function check_zone_change(new_id, old_id)
-    for _,chocobo_zone_id in pairs(chocobo_zone_ids) do
-        if new_id == chocobo_zone_id then
-            windower.send_command(string.format('timers c "Chocobo Area Delay" %d down', settings.delay.area + settings.delay.lag))
-            return
-        end
+function check_zone_change(new_zone_id, old_zone_id)
+    if chocobo_zone[new_zone_id] then
+        windower.send_command(string.format('timers c "Chocobo Area Delay" %d down', settings.delay.area + settings.delay.lag))
     end
 end
 
 function check_incoming_chunk(id, original, modified, injected, blocked)
     if id == 0x2A and windower.ffxi.get_player().id == original:unpack('I', 5) then
-        message_id = original:unpack('H', 27) % 0x8000
-        for _,success_message_id in pairs(success_message_ids) do
-            if message_id == success_message_id and get_chocobo_buff() then
-                update_stats(-1)
-                display_stats()
-                return
-            end
-        end
-        for _,ease_message_id in pairs(ease_message_ids) do
-            if message_id == ease_message_id then
-                update_stats(1)
-            end
+        local message_id = original:unpack('H', 27) % 0x8000
+        if success_message[message_id] and get_chocobo_buff() then
+            update_stats(-1)
+            display_stats()
+        elseif ease_message[message_id] then
+            update_stats(1)
         end
     elseif id == 0x2F and settings.delay.dig > 0 and windower.ffxi.get_player().id == original:unpack('I', 5) then
         windower.send_command(string.format('timers c "Chocobo Dig Delay" %d down', settings.delay.dig))
     elseif id == 0x36 and windower.ffxi.get_player().id == original:unpack('I', 5) then
-        message_id = original:unpack('H', 11) % 0x8000
-        for _,fail_message_id in pairs(fail_message_ids) do
-            if message_id == fail_message_id then
-                update_stats(0)
-                display_stats()
-            end
+        local message_id = original:unpack('H', 11) % 0x8000
+        if fail_message[message_id] then
+            update_stats(0)
+            display_stats()
         end
     end
 end
@@ -142,7 +147,7 @@ function digger_command(...)
         settings.accuracy.total = 0
         settings:save('all')
     elseif #arg == 2 and arg[1]:lower() == 'rank' then
-        rank = arg[2]:lower()
+        local rank = arg[2]:lower()
         if rank == 'amateur' then
             settings.delay.area = 60
             settings.delay.dig = 15
