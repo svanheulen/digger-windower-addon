87423f139475e9bc54080e5aa57ecfabc1cc58ab
svanheulen <svanheulen@gmail.com>
2014-06-18 16:47:02 -0400

    moved message script to a folder and added a readme

---
+++ messages/settings.py
@@ -0,0 +1,35 @@
+search = {
+    'fail': b'You dig and you dig, but find nothing.\x7f1\x00\x07',
+    'success': b'Obtained: \x7f\x80\x01\x01\x05#\x82\x80\x80\x80.\x7f1\x00\x07',
+    'full': b'You dig up \x01\x01\x01 \x01\x05$\x82\x80\x80\x80, but your inventory is full.\x07You regretfully throw the \x01\x05#\x82\x80\x80\x80 away.\x7f1\x00\x07',
+    'ease': b'It appears your chocobo found this item with ease.\x7f1\x00\x07'
+}
+
+zones = {
+    2: 6422, # carpenters' landing
+    4: 6424, # bibiki bay
+    51: 6471, # wajaom woodlands
+    52: 6472, # bhaflau thickets
+    100: 6520, # west ronfaure
+    101: 6521, # east ronfaure
+    102: 6522, # la theine plateau
+    103: 6523, # valkurm dunes
+    104: 6524, # jugner forest
+    105: 6525, # batallia downs
+    106: 6526, # north gustaberg
+    107: 6527, # south gustaberg
+    108: 6528, # konschtat highlands
+    109: 6529, # pashhow marshlands
+    110: 6530, # rolanberry fields
+    114: 6534, # eastern altepa desert
+    115: 6535, # west sarutabaruta
+    116: 6536, # east sarutabaruta
+    117: 6537, # tahrongi canyon
+    118: 6538, # buburimu peninsula
+    119: 6539, # meriphataud mountains
+    120: 6540, # sauromugue champaign
+    121: 6541, # the sanctuary of zi'tah
+    123: 6543, # yuhtunga jungle
+    124: 6544, # yhoator jungle
+    125: 6545, # western altepa desert
+}
